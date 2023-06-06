52bbd41b269974e1a462a19dc0161b6e1c6f4304
Min Yan <>
2023-06-07 05:42:43 +0800

    Simplify DBDataLoader logic with SQLAlchemy library

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from dotenv import dotenv_values
 import pandas as pd
-import pymysql
+from sqlalchemy import create_engine
 
 
 class DataLoader(ABC):
@@ -24,41 +24,29 @@ class CSVDataLoader(DataLoader):
 
 
 class DBDataLoader(DataLoader):
-    '''OOP-styled code adapted from Lesson 02 Notebook'''
+    '''
+        OOP-styled code for loading dataset from database.
+        Since queries are read-only, we can use an alternative method
+        to the one from Lesson 02 Notebook, which is easier to read.
+    '''
 
-    config = dotenv_values()  # Load secrets from .env file
+    config = dotenv_values()  # Bonus: Load secrets from external .env file
 
     def __init__(self):
-        self.conn = self.connect_to_database()
+        self.database = self._get_database_engine()
         self.query = 'SELECT * FROM loans'
 
     def load(self, query=None):
         # If custom SQL query not provided, use default query
         if query is None:
             query = self.query
+        print('Loading dataset from database...')
+        return pd.read_sql(query, self.database)
 
-        try:
-            with self.conn.cursor() as cursor:
-                cursor.execute(query)
-            self.conn.commit()
-            return pd.DataFrame(cursor.fetchall())
-        except Exception as ex:
-            print(f'Error when retrieving records: {ex}')
-            raise ex
+    def _get_database_engine(self):
+        host = self.config.get('DB_HOST')
+        user = self.config.get('DB_USER')
+        password = self.config.get('DB_PWD')
+        db = self.config.get('DB_NAME')
 
-    def connect_to_database(self):
-        try:
-            connection = pymysql.connect(
-                host=self.config.get('DB_HOST'),
-                port=self.config.get('DB_PORT', 3306),
-                user=self.config.get('DB_USER'),
-                passwd=self.config.get('DB_PWD'),
-                db=self.config.get('DB_NAME'),
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print('[+] Database Connection Successful.')
-            return connection
-
-        except Exception as ex:
-            print(f'[+] Database Connection Failed: {ex}')
-            raise ex
+        return create_engine(f'mysql+pymysql://{user}:{password}@{host}/{db}')
